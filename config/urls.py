d8fe39786142e016396c972220ce86b4f5f6eb95
dxrkn1ght <komronbekzubaydullayev@gmail.com>
2025-08-21 10:46:19 +0500

    upgraded for better performance

--- config/urls.py
+++ config/urls.py
@@ -1,65 +1,20 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import include, path, re_path
-from drf_yasg import openapi
-from drf_yasg.generators import OpenAPISchemaGenerator
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from django.urls import include, path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
 
-DJANGO_SETTINGS_MODULE = getattr(settings, "DJANGO_SETTINGS_MODULE", "development")
-
-
-class BothHttpAndHttpsSchemaGenerator(OpenAPISchemaGenerator):
-    def get_schema(self, request=None, public=False):
-        schema = super().get_schema(request, public)
-        schema.schemes = (
-            ["http"] if DJANGO_SETTINGS_MODULE == "development" else ["https"]
-        )
-        return schema
-
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="dxrkn1ght's Project API",
-        default_version="v1",
-        description="API for online store website",
-        terms_of_service="https://webresumekomron/terms/",
-        contact=openapi.Contact(email="info@kevin.uz"),
-        license=openapi.License(name="MIT License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-    generator_class=BothHttpAndHttpsSchemaGenerator,
-)
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/v1/common", include("apps.common.urls", namespace="common")),
+    path("api/v1/accounts", include("apps.accounts.urls", namespace="accounts")),
+    path("api/v1/store", include("apps.store.urls", namespace="store")),
+
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("swagger/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="schema-redoc"),
 ]
 
-urlpatterns += [
-    path("api/v1/common/", include("apps.common.urls"), name="common"),
-    path("api/v1/accounts/", include("apps.accounts.urls"), name="accounts"),
-    path("api/v1/store/", include("apps.store.urls"), name="store"),
-]
-
-if DJANGO_SETTINGS_MODULE == "development":
-    urlpatterns += [
-        path("__debug__/", include("debug_toolbar.urls")),
-        path(
-            "swagger/",
-            schema_view.with_ui("swagger", cache_timeout=0),
-            name="schema-swagger-ui",
-        ),
-        path(
-            "redoc/",
-            schema_view.with_ui("redoc", cache_timeout=0),
-            name="schema-redoc",
-        ),
-        re_path(
-            r"^swagger(?P<format>\.json|\.yaml)$",
-            schema_view.without_ui(cache_timeout=0),
-            name="schema-json",
-        ),
-    ]
+if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
